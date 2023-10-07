da73fda7f2762d1b4c2748e97e695ddf3d74e700
keepflowing <132106900+keepflowing@users.noreply.github.com>
2023-10-06 19:05:10 -0500

    Implement merge sort

--- app.js
+++ app.js
@@ -23,3 +23,31 @@ function fibRec (num) {
 for (let i in fibArr) {
   document.body.innerHTML += fibArr[i] + '<br>';
 } */
+
+// helper function for mergeSort
+function merge(left, right) {
+  const sortedArr = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      sortedArr.push(left.shift());
+    } else {
+      sortedArr.push(right.shift());
+    }
+  }
+
+  return [...sortedArr, ...left, ...right]
+}
+
+function mergeSort(arr) {
+  if(arr.length <= 1) {
+    return arr;
+  } else {
+    const mid = Math.floor(arr.length/2);
+
+    const left = mergeSort(arr.slice(0,mid));
+    const right = mergeSort(arr.slice(mid));
+
+    return merge(left, right);
+  }
+}
